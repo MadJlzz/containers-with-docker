0b6ebb63d9b5ca75b788afcc5287c43b31396b3e
Julien Klaer <j.klaer@tadaweb.com>
2024-11-12 22:18:20 +0100

    fix: startup script not working - creating vm as preemtible

--- demo/terraform/main.tf
+++ demo/terraform/main.tf
@@ -39,7 +39,7 @@ resource "google_compute_instance" "docker-training-demo" {
 
   metadata = {
     ssh-keys       = "jklaer:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxG38J4LgCDq6LiKfIRMxhG1giNifZmVNXsUUQsiws550IEXq59/p17RVevryKUTa1U8fz8+ATdHXAHIL/Hslj2kkBahniIoFYsAVHDBZEdbQjr88gEBanL7gaCbd7v0M7LgzVA1wRnswhr/vRXVkUSWIeAomUryLHbzJjQlCZWE5PcehFwTGu/A6njz7iXJyfFfyUkWLbYrNratNR963cU8Y5FH7Gspw24YDs/5lifBCT4kFl02u5j3YWq+HL0KkM78T6BQRqwZhcHUeR3dPLaNQCWkjzwxXzELf2SrKHfYHMs3X1kvEyBReMoWMdEZcfVv0d5b0pkdtnv1rrLytX+gtstmH2JX0VRRZihpsb9QlFss1W2R4FapcoCrxslPDvi+v9bkRoNIM2xnewZDVRd4xXrBi5Wp5U20SyRA4gIe3aCEDDXAwjNeuFBw8a/M4DhRhcod79iIozMuGZAx8gOYqzXmvfJx87M7aYru/GkRsDX6FB+4l+D6Jgq2GEwl9ekXmGmvZqN1emUyHQVXaSX36pwAlZyD6XjYTGn/2MCRVhCP/Br94QC7KIa0/tsQJHNDRsEYvAmTKZ239igcy1AqQDuSoA3COudSjUHGp0g7ButIIyBsoJflc4opCkB9/ivaJNFtTAOMKrCY7o3Ol3jy7zlRSu/WwC/L+w5a47AQ== jklaer@jklaer-XPS-13-9305"
-    startup-script = " #! /bin/bash\napt update\napt install docker-ce docker-ce-cli containerd.io -y"
+    startup-script = " #! /bin/bash\napt update\napt install docker.io -y\nusermod -a -G docker jklaer"
   }
 
   name = "docker-training-demo"
@@ -57,7 +57,7 @@ resource "google_compute_instance" "docker-training-demo" {
   scheduling {
     automatic_restart   = false
     on_host_maintenance = "TERMINATE"
-    preemptible         = false
+    preemptible         = true
     provisioning_model  = "SPOT"
   }
 
