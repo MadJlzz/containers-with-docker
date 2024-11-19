5dfaffa78ff9490c7c67ff7e145dfffd226301f2
Julien Klaer <j.klaer@tadaweb.com>
2024-11-19 17:27:25 +0100

    feat: removing spot machine and adding more patate to avoid disruption during the lesson

--- demo/terraform/main.tf
+++ demo/terraform/main.tf
@@ -35,7 +35,7 @@ resource "google_compute_instance" "docker-training-demo" {
     goog-ec-src = "vm_add-tf"
   }
 
-  machine_type = "e2-medium"
+  machine_type = "e2-standard-2"
 
   metadata = {
     ssh-keys       = "jklaer:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxG38J4LgCDq6LiKfIRMxhG1giNifZmVNXsUUQsiws550IEXq59/p17RVevryKUTa1U8fz8+ATdHXAHIL/Hslj2kkBahniIoFYsAVHDBZEdbQjr88gEBanL7gaCbd7v0M7LgzVA1wRnswhr/vRXVkUSWIeAomUryLHbzJjQlCZWE5PcehFwTGu/A6njz7iXJyfFfyUkWLbYrNratNR963cU8Y5FH7Gspw24YDs/5lifBCT4kFl02u5j3YWq+HL0KkM78T6BQRqwZhcHUeR3dPLaNQCWkjzwxXzELf2SrKHfYHMs3X1kvEyBReMoWMdEZcfVv0d5b0pkdtnv1rrLytX+gtstmH2JX0VRRZihpsb9QlFss1W2R4FapcoCrxslPDvi+v9bkRoNIM2xnewZDVRd4xXrBi5Wp5U20SyRA4gIe3aCEDDXAwjNeuFBw8a/M4DhRhcod79iIozMuGZAx8gOYqzXmvfJx87M7aYru/GkRsDX6FB+4l+D6Jgq2GEwl9ekXmGmvZqN1emUyHQVXaSX36pwAlZyD6XjYTGn/2MCRVhCP/Br94QC7KIa0/tsQJHNDRsEYvAmTKZ239igcy1AqQDuSoA3COudSjUHGp0g7ButIIyBsoJflc4opCkB9/ivaJNFtTAOMKrCY7o3Ol3jy7zlRSu/WwC/L+w5a47AQ== jklaer@jklaer-XPS-13-9305"
@@ -52,13 +52,6 @@ resource "google_compute_instance" "docker-training-demo" {
     queue_count = 0
     stack_type  = "IPV4_ONLY"
     subnetwork  = "projects/madsandbox/regions/europe-west1/subnetworks/default"
-  }
-
-  scheduling {
-    automatic_restart   = false
-    on_host_maintenance = "TERMINATE"
-    preemptible         = true
-    provisioning_model  = "SPOT"
   }
 
   shielded_instance_config {
