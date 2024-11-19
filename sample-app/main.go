fbc7543007ae5d864bec91bf679a185c52d4c34a
Julien Klaer <j.klaer@tadaweb.com>
2024-11-19 18:28:20 +0100

    feat: calling pokemon api for a simple route to test

--- sample-app/main.go
+++ sample-app/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
+
+const PokemonApi = "https://pokeapi.co/api/v2"
 
 func main() {
 	cfg, err := NewConfiguration()
@@ -15,6 +18,19 @@ func main() {
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "ok")
+	})
+
+	mux.HandleFunc("/api/v1/pokemon/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := r.PathValue("name")
+		resp, err := http.Get(fmt.Sprintf("%s/pokemon/%s", PokemonApi, name))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, "Something went wrong")
+		}
+		defer resp.Body.Close()
+
+		w.Header().Add("Content-Type", "application/json")
+		io.Copy(w, resp.Body)
 	})
 
 	fmt.Printf("server started and listening on %s\n", cfg.Server.Hostname)
